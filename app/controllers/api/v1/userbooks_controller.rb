f3449d4d4abefd64acc2615e58833e9e55cb6351
github username <github email address>
2017-06-28 14:40:46 -0400

    user_add

--- app/controllers/api/v1/userbooks_controller.rb
+++ app/controllers/api/v1/userbooks_controller.rb
@@ -1,9 +1,18 @@
-class Api::V1::UserBooksController < ApplicationController
+class Api::V1::UserbooksController < ApplicationController
 
   def index
     userBooks = UserBook.all
     render json: userBooks
   end
 
+  def create
+    userBook = UserBook.create(userbook_params)
+    render json: userBook
+  end
+
+  private
+  def userbook_params
+    params.require(:userbook).permit(:book_id, :user_id)
+  end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module LendrApi
 
 
     config.api_only = true
-
+      config.middleware.use ActionDispatch::Flash
       config.middleware.insert_before 0, Rack::Cors do
        allow do
          origins '*'
