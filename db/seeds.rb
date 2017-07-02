81907154cd0fd8086adceea6b310e31de20a46c4
github username <github email address>
2017-07-02 13:23:37 -0400

    addedDetailsToUserModel

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,12 @@
 require 'faker'
 
 20.times do
-  User.create(username: Faker::Internet.unique.user_name, password: 'pw', email: Faker::Internet.email, picture: 'Projects/Lendaroo/lendr_api/public/smiley.jpg', home_location: Faker::Address.latitude + ',' + Faker::Address.longitude
+  User.create(username: Faker::Internet.unique.user_name, password: 'pw', email: Faker::Internet.email, bio: Faker::Hipster.sentence, picture: 'http://graphiccave.com/wp-content/uploads/2015/03/Smile-PNG.png', home_location: Faker::Address.latitude + ',' + Faker::Address.longitude
   )
 end
 
 20.times do
-  Book.create(title: Faker::Book.title, author: Faker::Book.author, description: Faker::Hipster.paragraph, image_url: 'Projects/Lendaroo/lendr_api/public/smiley.jpg', rating: Faker::Number.between(1, 5).to_i)
+  Book.create(title: Faker::Book.title, author: Faker::Book.author, description: Faker::Hipster.paragraph, image_url: 'http://www.freeiconspng.com/uploads/book-icon-black-good-galleries--24.jpg', rating: Faker::Number.between(1, 5).to_i)
 end
 
 
