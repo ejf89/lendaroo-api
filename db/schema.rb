2e63f50bfbc4dc31ed135302ac0d802102beade3
github username <github email address>
2017-07-06 13:30:44 -0400

    crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705151945) do
+ActiveRecord::Schema.define(version: 20170705204642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170705151945) do
     t.string "status", default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "user_books", force: :cascade do |t|
